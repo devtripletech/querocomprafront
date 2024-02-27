c22e8bf97563491b23f409fb597ad5f67da70c46
Edvan Lima <edvan.lima@tripletech.com.br>
2024-02-27 10:48:16 -0300

    ajustar cache

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -34,18 +34,18 @@ export default async function IndexPage() {
 
   const limit = 8
   const offset = 0
-  const allProducts = await listProductsWithParamsAction({ limit, offset })
-  // const someProducts = await cache(
-  //   async () => {
+  const someProducts = await cache(
+    async () => {
+      const allProducts = await listProductsWithParamsAction({ limit, offset })
 
-  //     return allProducts.produtos
-  //   },
-  //   ["lobby-products"],
-  //   {
-  //     revalidate: 3600,
-  //     tags: ["lobby-products"],
-  //   }
-  // )()
+      return allProducts.produtos
+    },
+    ["lobby-products"],
+    {
+      revalidate: 3600,
+      tags: ["lobby-products"],
+    }
+  )()
 
   return (
     <Shell className="max-w-6xl pt-0 md:pt-0">
@@ -82,7 +82,7 @@ export default async function IndexPage() {
                 <ProductCardSkeleton key={i} />
               ))}
             >
-              {allProducts.produtos.map((product: any) => (
+              {someProducts.map((product: any) => (
                 <ProductCard key={product.id_produto} product={product} />
               ))}
             </React.Suspense>
